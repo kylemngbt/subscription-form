7feba75e4fbd4c188e35e030316e0e301631763d
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-15 20:28:58 +0800

    build: create a reference to the span text

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,6 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwX-k4a-OYBzQiCQ_pu1bo9OhLBhkdfgV7-z-jvL38pdE6QgGBUuYgdmdmkbmsYTDqy/exec'
 const form = document.forms['submit-to-google-sheet']
+const msg = document.querySelector("#msg");
 
 form.addEventListener('submit', e => {
   e.preventDefault()
